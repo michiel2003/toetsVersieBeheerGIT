956470d315578a30db6651d95ccb062a478a76ea
michiel2003 <michieldecap@gmail.com>
2020-01-22 11:20:24 +0100

    new version

--- ArrayPrinter.java
+++ ArrayPrinter.java
@@ -1,6 +1,6 @@
 
 public class ArrayPrinter {
-	
+		
 	public static void main(String args[]) {
 		int[] array = {12, 5, 4, 8, 9};
 		ArrayPrinter exec = new ArrayPrinter();
@@ -8,6 +8,9 @@ public class ArrayPrinter {
 	}
 	
 	public void printarray(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 	}
 	
 }
