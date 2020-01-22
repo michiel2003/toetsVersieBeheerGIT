ac1e98212c93071aa79285b01afe9a1f429dbb79
michiel2003 <michieldecap@gmail.com>
2020-01-22 11:18:21 +0100

    first commit

---
+++ ArrayPrinter.java
@@ -0,0 +1,13 @@
+
+public class ArrayPrinter {
+	
+	public static void main(String args[]) {
+		int[] array = {12, 5, 4, 8, 9};
+		ArrayPrinter exec = new ArrayPrinter();
+		exec.printarray(array);
+	}
+	
+	public void printarray(int[] array) {
+	}
+	
+}
